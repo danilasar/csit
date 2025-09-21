b3d517ea9aa212f35e6afa5485cbbdb01c8f5089
Данила Григорьев <danila.sar@yandex.ru>
2025-09-21 21:13:21 +0400

    Взлетаем

---
+++ src/client/java/ru/sgu/CSITClient.java
@@ -0,0 +1,10 @@
+package ru.sgu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CSITClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
